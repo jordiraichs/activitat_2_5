256ad2625781b9a31e06da043ecd421c449c82c8
jordi <jraichs@inscaparrella.cat>
2023-11-17 13:24:45 +0100

    vesio final

--- app/src/main/java/com/dam_m8/bd/MainActivity2.kt
+++ app/src/main/java/com/dam_m8/bd/MainActivity2.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,27 @@ class MainActivity2 : AppCompatActivity() {
             val intent = Intent(this@MainActivity2, MainActivity::class.java)
             startActivity(intent)
         }
-    }
 
+        val bproductes: Button = findViewById(R.id.btproductes)
+        bproductes.setOnClickListener{
+            val admin = DatabaseHandler(this,"tenda.db",null,1)
+            val db = admin.readableDatabase
+            val linearLayout: LinearLayout = findViewById(R.id.layout01)
+
+            val registres = db.rawQuery("SELECT *  FROM productes",null)
+            if (registres.moveToFirst()) {
+                while(!registres.isAfterLast) {
+                    val vtext = TextView(this)
+                    vtext.text = registres.getString(0) + " -- " + registres.getString(1)
+                    linearLayout.addView(vtext)
+                    registres.moveToNext()
+                }
+            } else {
+                Toast.makeText(this, "La base de dades és buida",  Toast.LENGTH_SHORT).show()
+            }
+
+            db.close()
+            registres.close() // alliberem el cursor despres de tancar BD com indica ajuda
+        }
+    }
 }
